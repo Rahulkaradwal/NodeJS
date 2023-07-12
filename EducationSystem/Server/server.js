8ce5cfbd205ce2243a6de9ec471f8d5119d0fd9a
rahulkaradwal <rahulkaradwal2000@gmail.com>
2023-07-12 17:36:21 -0400

    setting up the connection with mongo

--- EducationSystem/Server/server.js
+++ EducationSystem/Server/server.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const mongoURI =
+  "mongodb+srv://rahulkaradwal:14%40February@cluster0.4cjd0lx.mongodb.net/mydatabase?retryWrites=true&w=majority";
+
+const dbName = "mydatabase";
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.get("/", (req, res) => {
   // Handle root URL request
